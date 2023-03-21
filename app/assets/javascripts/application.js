ec244367a7216feab789202695e7a156d86e2dd1
Hein Tay Zar <heintayzarhm@gmail.com>
2023-03-21 18:11:27 +0630

    Add nav bar

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeChanger = document.querySelector('.theme-changer');
+    console.log("themeChanger", themeChanger)
+    const toggleDarkMode = () => {
+        document.documentElement.classList.toggle('dark');
+        themeChanger.children[0].classList.toggle('hidden');
+        themeChanger.children[1].classList.toggle('hidden');
+        localStorage.setItem('darkMode', document.documentElement.classList.contains('dark'));
+    }
+
+    if (localStorage.getItem('darkMode') === 'true') {
+        toggleDarkMode();
+    }
+    themeChanger.addEventListener('click',toggleDarkMode);
+
+  });
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -81,8 +81,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-    :user_name =>     "heintayzartech@gmail.com",
-    :password =>    "hevbblcnzmzzwgyq",
+    :user_name =>     "fakerhein@gmail.com",
+    :password =>    "kxstyelfpfdautaa",
     :domain =>     "google.com",
     :address =>      'smtp.gmail.com',
     :port =>     587,
